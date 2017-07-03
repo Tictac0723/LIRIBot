854acb2884791bd9281009c15a5b361e1ec37ebe
Tictac0723 <taylorclark0723@gmail.com>
2017-07-03 13:25:46 -0500

    it's alive!

--- keys.js
+++ keys.js
@@ -1,8 +1,13 @@
 console.log("this is loaded");
 
 exports.twitterKeys = {
-    consumer_key: 'GesooJEYEy8kvE6b5Pngsdfjt',
-    consumer_secret: '1gzJYSFwrtMdl4o0iEKMqSVuREAR5HnKYGYiOg5eeWgHMEbCmt',
+    consumer_key: 'OaZcyRYbVbbiAqVHzpepg8zvg',
+    consumer_secret: 'skMEtGNxDDIU5ENVOedrmAbw7K2wwRht00MAfLMyjT4QxgbV5P',
     access_token_key: '880568392008306688-1z8oxtnD2rraaX3W2wVirNKFtX9d3ei',
     access_token_secret: 'AtX9KyIULShhA2hllE955Wf6c0OamKOLlNEMVAJtwgZRi'
+}
+
+exports.spotifyKeys = {
+	id: "0a16a720a8f84c878893961b1321687d",
+	secret: "bb8d04a624ca422aa016be330d8239bf"
 }
--- liri.js
+++ liri.js
@@ -1,23 +1,25 @@
 var action = process.argv[2];
 var value = process.argv[3];
-var keys = require("./keys.js")
+var keys = require("./keys.js");
 var twitter = require("twitter");
-var client = new twitter("keys.twitterKeys");
+var client = new twitter(keys.twitterKeys);
 var params = {
     screen_name: "tictac_unicorn",
     count: 20
 }
-var spotify = require("spotify");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotifyKeys);
 var request = require("request");
 var fs = require("fs");
 
 switch (action) {
     case "my-tweets":
-        twitter();
+    console.log("here");
+        invokeTwitter();
         break;
 
     case "spotify-this-song":
-        spotify();
+       invokeSpotify();
         break;
 
     case "movie-this":
@@ -30,10 +32,36 @@ switch (action) {
 }
 
 
-function twitter() {
-    client.get("statuses/user_timeline", params, function(error, tweets, response) {
+function invokeTwitter() {
+    twitterClient.get("statuses/user_timeline", params, function(error, tweets, response) {
         if (!error && response.statusCode == 200) {
-            console.log(tweets);
+            fs.writeFile('log.txt', "");
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
+                console.log(tweets[i].created_at);
+                fs.appendFile('log.txt', ("Tweet: " + tweets[i].text + "Date & Time: " + tweets[i].created_at + "\r\n"))
+            }
+        }
+        else {
+            console.log(error);
+        }
+    })
+}
+
+function invokeSpotify() {
+    spotify.search({ type: 'track', query: value}, function(error, data){
+        if (error){
+            return console.log("Error occurred: " + error);
+        }
+        else {
+            fs.writeFile('log.txt', "");
+            var firstObject = data.tracks.items[0];
+            console.log(firstObject.artists[0].name);
+            console.log(firstObject.name);
+            console.log(firstObject.external_urls.spotify);
+            console.log(firstObject.album.name);
+            fs.appendFile('log.txt', ("Song Title: " + firstObject.name + "\r\n" + "Artist Name: " + firstObject.artists[0].name  + "\r\n" + "Listen: " + firstObject.external_urls.spotify + "\r\n" + "Album Name: " + firstObject.album.name +  "\r\n"))
+
         }
     })
 }
@@ -52,6 +80,17 @@ function movie() {
             console.log("Language: " + JSON.parse(body).Language);
             console.log("Plot: " + JSON.parse(body).Plot);
             console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    })
+}
+
+function random(){
+    fs.readFile("random.txt", "utf8", function(error, data){
+        if (error){
+            return console.log(error);
+        }
+        else {
+            console.log(data);
         }
     })
 }
